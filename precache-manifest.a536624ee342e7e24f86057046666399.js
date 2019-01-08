0d7f2a15cdbe951e0d9f35f16cd02adad760f22a
Jason Lo <github@jasonlo.email>
2019-01-07 19:49:43 -0800

    Updates

---
+++ precache-manifest.a536624ee342e7e24f86057046666399.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7341d617f43dd680e13a",
+    "url": "/now-playing/static/css/main.b3115923.chunk.css"
+  },
+  {
+    "revision": "7341d617f43dd680e13a",
+    "url": "/now-playing/static/js/main.7341d617.chunk.js"
+  },
+  {
+    "revision": "9fadddde60644e8bf745",
+    "url": "/now-playing/static/js/1.9fadddde.chunk.js"
+  },
+  {
+    "revision": "18008ddcb6968cb2f398",
+    "url": "/now-playing/static/js/runtime~main.18008ddc.js"
+  },
+  {
+    "revision": "35bd3042677ba59d6941a415b133ab4f",
+    "url": "/now-playing/index.html"
+  }
+];
